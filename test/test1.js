29906e1f060883f18442a0d5b52ea2c031579e46
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-12-31 15:03:55 +0200

    counter function for elements

--- test/test1.js
+++ test/test1.js
@@ -1,1 +1,12 @@
 console.log("running...");
+var search = function (elements, element) {
+    var i, counter = 0;
+    for (i = 0; i < elements.length; i++) {
+        if (elements[i] == element)
+            counter++;
+    }
+    return counter;
+};
+var numbers = [1, 2, 3, 1, 2, 4, 1];
+var searchedNumber = 1;
+console.log(search(numbers, searchedNumber));
--- test/test1.ts
+++ test/test1.ts
@@ -1,1 +1,13 @@
 console.log("running...");
+
+let search : (allElements: Array<any>, searchedElement: any) => number = function (elements, element) {
+    let i, counter = 0;
+    for (i = 0; i < elements.length; i++) {
+        if (elements[i] == element) counter++;
+    }
+    return counter;
+}
+
+let numbers = [1, 2, 3, 1, 2, 4, 1];
+let searchedNumber = 1;
+console.log(search(numbers, searchedNumber));
