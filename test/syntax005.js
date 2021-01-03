dbf41894036cd991b0b9e76aaa64c6bdfab5aa6a
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-01-04 00:08:02 +0200

    package calling

---
+++ test/syntax005.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var syntax004_1 = require("./syntax004");
+var result = syntax004_1.moo.search([2, 2, 3, 2, 2, 4, 2], 2);
+console.log(result);
---
+++ test/syntax005.ts
@@ -0,0 +1,4 @@
+import { moo } from "./syntax004";
+
+let result = moo.search([2, 2, 3, 2, 2, 4, 2], 2);
+console.log(result);
