58759660c33b018969ef62c4484f52033c698329
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-02-20 00:02:13 +0200

    started template work

--- server2/build/src/templates/templates.js
+++ server2/build/src/templates/templates.js
@@ -1,3 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.template = void 0;
+var template;
+(function (template) {
+    class TemplateRenderer {
+    }
+    template.TemplateRenderer = TemplateRenderer;
+    class PlainTextRenderer {
+        constructor(text) {
+            this.text = text;
+        }
+        render() {
+            return this.text;
+        }
+    }
+    template.PlainTextRenderer = PlainTextRenderer;
+    class JsonRenderer {
+    }
+    template.JsonRenderer = JsonRenderer;
+})(template = exports.template || (exports.template = {}));
 //# sourceMappingURL=templates.js.map
--- server2/src/templates/templates.ts
+++ server2/src/templates/templates.ts
@@ -2,4 +2,24 @@ export module template {
     export interface TemplateRendererInterface {
 
     }
+
+    export class TemplateRenderer implements TemplateRendererInterface {
+
+    }
+
+    export class PlainTextRenderer implements TemplateRendererInterface {
+        text: string;
+
+        constructor(text: string) {
+            this.text = text;
+        }
+
+        render() {
+            return this.text;
+        }
+    }
+
+    export class JsonRenderer implements TemplateRendererInterface {
+
+    }
 }
